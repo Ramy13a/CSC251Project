4072b9344e23e36ed66f31604a5b3c5cf9ddbfcc
Ramy13a <ramycheaib2@yahoo.com>
2023-09-11 08:50:32 -0400

    Update Policy.java

--- Policy.java
+++ Policy.java
@@ -16,12 +16,13 @@ public class Policy
       policyNumber = 0;  
    }
    
-   
+   //@param policyNumber setting policy Number to another variable
    public Policy (int p)
    {
       policyNumber = p;
    }
-         
+   //single constructor
+   //@param age is the age of the provider     
    public void setAge(int age)
    {
       this.age = age;
@@ -29,8 +30,9 @@ public class Policy
    public int getAge()
    {
       return this.age;
-   }        
-   
+   }  
+   //single constructor      
+   //@param height is the height of the provider
    public void setHeight(int height)
    {
       this.height = height;
@@ -38,8 +40,9 @@ public class Policy
    public int getHeight()
    {
       return this.height;
-   }        
-   
+   }    
+   //single constructor    
+   //@param providerName is the name of the provider
    public void setProviderName(String providerName)
    {
       this.providerName = providerName;
@@ -47,8 +50,9 @@ public class Policy
    public String getProviderName()
    {
       return providerName;
-   }       
-   
+   }  
+   //single constructor     
+   //@param firstName is the first name of the provider
    public void setFirstName(String firstName)
    {
       this.firstName = firstName;
@@ -56,8 +60,9 @@ public class Policy
    public String getFirstName()
    {
       return this.firstName;
-   }        
-   
+   }  
+   //single constructor      
+   //@param lastName is the last name of the provider
    public void setLastName(String lastName)
    {
       this.lastName = lastName;
@@ -65,8 +70,9 @@ public class Policy
    public String getLastName()
    {
       return this.lastName;
-   }        
-   
+   }  
+   //single constructor      
+   //@param smokingStatus is the status the smoker chooses he's in
    public void setSmokingStatus(String smokingStatus)
    {
       this.smokingStatus = smokingStatus;
@@ -74,8 +80,9 @@ public class Policy
    public String getSmokingStatus()
    {
       return this.smokingStatus;
-   }       
-   
+   }  
+   //single constructor     
+   //@param weight is the weight of the provider
    public void setWeight(double weight)
    {
       this.weight = weight;
@@ -83,8 +90,8 @@ public class Policy
    public double getWeight()
    {
       return this.weight;
-   }        
- 
+   } //two constructors      
+  //@param BMI is the formula to measure BMI 
    public double getBMI()
    {
       double w = getWeight();
@@ -92,7 +99,8 @@ public class Policy
       final double BMI = ( w * 703 ) / ( h*h); 
       return BMI;
    }
-   
+   //four constructors
+   //@param getInsurancePrice is the price of insurance as a total
    public double getInsurancePrice()
    {
       double totalPrice = 0;
@@ -101,7 +109,7 @@ public class Policy
       String s = getSmokingStatus();
       double bmi = getBMI();
       double additional = 0;
-      if(a > 50)
+      if(a > 50) // if statement if they're older than 50
       {
          totalPrice = baseFee + 75;
       }
@@ -109,7 +117,7 @@ public class Policy
       {
          totalPrice = baseFee;
       }
-      if(s == "smoker" )
+      if(s == "smoker" ) // if statement if they're a smoker 
       {
          totalPrice = baseFee + 100;
       }
@@ -118,7 +126,7 @@ public class Policy
          totalPrice = baseFee;
       }
      
-      if(bmi > 35)
+      if(bmi > 35) // if statement if their bmi is above 35
       {
          additional = (bmi - 35) * 20;
          totalPrice = additional + baseFee;
