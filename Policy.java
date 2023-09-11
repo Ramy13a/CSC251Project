b7e352c0650e01e14e73621109b3759169187678
Ramy13a <ramycheaib2@yahoo.com>
2023-09-11 00:43:30 -0400

    Modified program
    best so far

---
+++ Policy.java
@@ -0,0 +1,133 @@
+public class Policy 
+{
+   private int policyNumber,
+               age,
+               height;
+   private String providerName,
+                  firstName,
+                  lastName,
+                  smokingStatus;
+   private double weight;
+   
+   
+   // no-arg constructor
+   public Policy()
+   {
+      policyNumber = 0;  
+   }
+   
+   
+   public Policy (int p)
+   {
+      policyNumber = p;
+   }
+         
+   public void setAge(int age)
+   {
+      this.age = age;
+   }
+   public int getAge()
+   {
+      return this.age;
+   }        
+   
+   public void setHeight(int height)
+   {
+      this.height = height;
+   }
+   public int getHeight()
+   {
+      return this.height;
+   }        
+   
+   public void setProviderName(String providerName)
+   {
+      this.providerName = providerName;
+   }
+   public String getProviderName()
+   {
+      return providerName;
+   }       
+   
+   public void setFirstName(String firstName)
+   {
+      this.firstName = firstName;
+   }
+   public String getFirstName()
+   {
+      return this.firstName;
+   }        
+   
+   public void setLastName(String lastName)
+   {
+      this.lastName = lastName;
+   }
+   public String getLastName()
+   {
+      return this.lastName;
+   }        
+   
+   public void setSmokingStatus(String smokingStatus)
+   {
+      this.smokingStatus = smokingStatus;
+   }
+   public String getSmokingStatus()
+   {
+      return this.smokingStatus;
+   }       
+   
+   public void setWeight(double weight)
+   {
+      this.weight = weight;
+   }
+   public double getWeight()
+   {
+      return this.weight;
+   }        
+ 
+   public double getBMI()
+   {
+      double w = getWeight();
+      int h = getHeight();
+      final double BMI = ( w * 703 ) / ( h*h); 
+      return BMI;
+   }
+   
+   public double getInsurancePrice()
+   {
+      double totalPrice = 0;
+      final int baseFee = 600;
+      int a = getAge();
+      String s = getSmokingStatus();
+      double bmi = getBMI();
+      double additional = 0;
+      if(a > 50)
+      {
+         totalPrice = baseFee + 75;
+      }
+      else
+      {
+         totalPrice = baseFee;
+      }
+      if(s == "smoker" )
+      {
+         totalPrice = baseFee + 100;
+      }
+      else
+      {
+         totalPrice = baseFee;
+      }
+      if(bmi > 35)
+      {
+         additional = (bmi - 35) * 20;
+         totalPrice = additional + baseFee;
+      }
+      else
+      {
+         totalPrice = baseFee;
+      }
+      return totalPrice;
+   }
+
+
+}
--- Project_Ramy_Cheaib.java
+++ Project_Ramy_Cheaib.java
@@ -1,7 +1,67 @@
+import java.util.Scanner;
+
 public class Project_Ramy_Cheaib
 {
    public static void main(String[]args)
    {
+   // setting up variables
+   int policyNumber,
+               age,
+               height;
+   String providerName,
+          firstName,
+          lastName,
+          smokingStatus;
+   double weight;
       
+      Scanner keyboard = new Scanner(System.in);
+      // setters 
+      System.out.println("Please enter the Policy Number: ");
+      policyNumber = keyboard.nextInt();
+      Policy p = new Policy(policyNumber);
+      
+      System.out.println("Please enter the Provider Name: ");
+      providerName = keyboard.next(); 
+      p.setProviderName(providerName);
+      
+      System.out.println("Please enter the Policyholder’s First Name: ");
+      firstName = keyboard.next();
+      firstName = keyboard.next();
+      p.setFirstName(firstName);
+      
+      System.out.println("Please enter the Policyholder’s Last Name: ");
+      lastName = keyboard.next();
+      p.setLastName(lastName);
+      
+      System.out.println("Please enter the Policyholder’s Age: ");
+      age = keyboard.nextInt();
+      p.setAge(age);
+      
+      System.out.println("Please enter the Policyholder’s Smoking Status (smoker/non-smoker)");
+      smokingStatus = keyboard.next();
+      p.setSmokingStatus(smokingStatus);
+      
+      System.out.println("Please enter the Policyholder’s Height (in inches)");
+      height = keyboard.nextInt();
+      p.setHeight(height);
+      
+      System.out.println("Please enter the Policyholder’s Weight (in pounds):");
+      weight = keyboard.nextDouble();
+      p.setWeight(weight);
+      
+      
+     // getters 
+     System.out.println("Policy Number" + policyNumber);
+     System.out.println("Provider Name" + p.getProviderName());  
+     System.out.println("Policyholder’s First Name: " + p.getFirstName());
+     System.out.println("Policyholder’s Last Name: " + p.getLastName());
+     System.out.println("Policyholder’s Age: " + p.getAge());
+     System.out.println("Policyholder’s Smoking Status: " + p.getSmokingStatus());
+     System.out.println("Policyholder’s Height: " + p.getHeight());
+     System.out.println("Policyholder’s Weight: " + p.getWeight());    
+     System.out.println("Policyholder's BMI: " + p.getBMI());
+     System.out.println("Policy Price: " + p.getInsurancePrice());
+     
+
    }
 }
